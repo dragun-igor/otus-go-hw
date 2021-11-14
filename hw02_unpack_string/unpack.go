f33a843f9574a5eb6948bf0ad49aaf966af200dc
Igor Dragun <draggun.ia@gmail.com>
2021-11-14 05:30:25 +0300

    Extra task

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -7,91 +7,92 @@ import (
 	"unicode"
 )
 
-var ErrInvalidString = errors.New("invalid string")
+var (
+	ErrFirstChar     = errors.New("the first char must be letter, rune(92) or rune(10)")
+	ErrInvalidChar   = errors.New("invalid char")
+	ErrTwoDigit      = errors.New("two digit in a row")
+	ErrProtLetter    = errors.New("protected letter char")
+	ErrLastBackslash = errors.New("last rune is not protected backslash")
+	ErrInvalidString = errors.New("invalid string")
+)
 
-func digit(r rune, prevr rune, protected bool) (string, error) { // Функция вызывается при цифровой руне
-	// Если предыдущая руна - буква, перенос или экранированные слэш или цифра, то выполняем размножение и возвращаем строку
-	if unicode.IsLetter(prevr) || prevr == 10 || ((prevr == 92 || unicode.IsDigit(prevr)) && protected) { // r == 10 => "\n", r == 92 => "\"
-		num, _ := strconv.Atoi(string(r))
-		result := strings.Repeat(string(prevr), num)
-		return result, nil
+const (
+	BACKSLASH = 92
+	LINEFEED  = 10
+)
+
+type token struct {
+	r         rune
+	prev      *token
+	protected bool
+	digit     bool
+	letter    bool
+	backslash bool
+	linefeed  bool
+	first     bool
+	last      bool
+	out       string
+	err       error
+}
+
+func (token *token) Digit() (string, error) {
+	if token.first {
+		return "", ErrFirstChar
 	}
-	// Если предыдущая руна - неэкранированный слэш, то возвращаем пустую строку
-	if prevr == 92 && !protected {
-		return "", nil
+	if token.prev.digit && !token.prev.protected {
+		return "", ErrTwoDigit
 	}
-	// В остальных случаях возвращаем ошибку
+	num, _ := strconv.Atoi(string(token.r))
+	if token.protected {
+		return string(token.r), nil
+	}
+	if !(token.prev.backslash && !token.prev.protected) && num > 0 {
+		return strings.Repeat(string(token.prev.r), num-1), nil
+	}
 	return "", ErrInvalidString
 }
 
-func notdigit(r rune, prevr rune, protected bool) (string, error) { // Функция вызывается при нецифровой руне (буква, перенос, слэш)
-	// Если предыдущая руна - буква, то возвращаем букву
-	if unicode.IsLetter(prevr) {
-		return string(prevr), nil
+func (token *token) NotDigit() (string, error) {
+	if token.letter && token.protected {
+		return "", ErrProtLetter
 	}
-	// Если предыдущая руна - неэкранированная цифра, то возвращаем пустую строку, если - экранированная цифра, то возвращаем цифру
-	if unicode.IsDigit(prevr) {
-		if !protected {
-			return "", nil
-		} else {
-			return string(prevr), nil
-		}
+	if token.backslash && !token.protected && token.last {
+		return "", ErrLastBackslash
 	}
-	// Если предыдущая руна - перенос строки, то возвращаем перенос строки
-	if prevr == 10 {
-		return "\n", nil
+	if token.backslash && !token.protected {
+		return "", nil
 	}
-	// Если предыдущая руна экранированный слэш, то возвращаем слэш, если неэкранированый - пустую строку
-	if prevr == 92 {
-		if protected {
-			return "\\", nil
-		}
-		if !protected && r == 92 {
-			return "", nil
-		}
-	}
-	// В остальных случаях возвращаем ошибку
-	return "", ErrInvalidString
+	return string(token.r), nil
 }
 
 func Unpack(in string) (string, error) {
 	var result strings.Builder
-	var prevr rune
-	var err error
-	var str string
 	runes := []rune(in)
-	// Слайс значений экранирования
-	protected := make([]bool, len(runes))
+	tokens := make([]token, len(runes))
+	tokens[0].first = true
+	tokens[len(tokens)-1].last = true
 	for i, r := range runes {
-		// Если первая руна ни буква, ни перенос, ни слэш, то возвращаем ошибку
-		if !unicode.IsLetter(r) && r != 10 && r != 92 && i == 0 {
-			err = ErrInvalidString
+		tokens[i].r = r
+		tokens[i].digit = unicode.IsDigit(r)
+		tokens[i].letter = unicode.IsLetter(r)
+		tokens[i].backslash = r == BACKSLASH
+		tokens[i].linefeed = r == LINEFEED
+		if !tokens[i].digit && !tokens[i].letter && !tokens[i].backslash && !tokens[i].linefeed {
+			return "", ErrInvalidChar
 		}
-		// Если предыдущая руна - неэкранированный слэш - взводим флаг экранированирования
-		if prevr == 92 && !protected[i-1] {
-			protected[i] = true
+		if i > 0 {
+			tokens[i].prev = &tokens[i-1]
+			tokens[i].protected = tokens[i].prev.r == BACKSLASH && !tokens[i].prev.protected
 		}
-		if i != 0 {
-			if unicode.IsLetter(r) || r == 10 || r == 92 {
-				str, err = notdigit(r, prevr, protected[i-1])
-			}
-			if unicode.IsDigit(r) && i != 0 {
-				str, err = digit(r, prevr, protected[i-1])
-			}
+		if tokens[i].digit {
+			tokens[i].out, tokens[i].err = tokens[i].Digit()
+		} else {
+			tokens[i].out, tokens[i].err = tokens[i].NotDigit()
 		}
-		// Если руна - последняя и она - не неэкранированная цифра, то добавляем её к окончательному результату
-		if i == len(runes)-1 && !(!protected[i] && unicode.IsDigit(r)) {
-			str += string(r)
+		if tokens[i].err != nil {
+			return "", tokens[i].err
 		}
-		// Если руна - последняя и она - неэкранированный слэш, возвращаем ошибку
-		if i == len(runes)-1 && !protected[i] && r == 92 {
-			err = ErrInvalidString
-		}
-		if err != nil {
-			return "", err
-		}
-		result.WriteString(str)
-		prevr = r
+		result.WriteString(tokens[i].out)
 	}
 	return result.String(), nil
 }
