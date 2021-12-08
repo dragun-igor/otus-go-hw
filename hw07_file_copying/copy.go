8073ed27f9a8bf1f9d0f3fff986b2cc55703bbbd
Igor Dragun <dragun.msc@gmail.com>
2021-12-09 00:48:53 +0300

    .

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"errors"
-	"fmt"
 	"io"
 	"os"
 	"time"
@@ -19,23 +18,18 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Если смещение меньше 0 - ошибка
 	if offset < 0 {
 		return ErrIncorrectOffset
 	}
-	// Если ограничение меньше 0 - ошибка
 	if limit < 0 {
 		return ErrIncorrectLimit
 	}
-	// Открываем файл, возвращаем ошибку, перед возвратом закрываем файл, режим READ_ONLY
 	fromFile, err := os.OpenFile(fromPath, os.O_RDONLY, 0o740)
 	if err != nil {
 		return err
 	}
 	defer fromFile.Close()
-	// Если смещение больше длины файла - ошибка
-	// Если длина 0 - файл не поддерживается
-	// Если ограничение ноль, приравниваем длине файла
+
 	stat, err := fromFile.Stat()
 	if err != nil {
 		return err
@@ -46,43 +40,34 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 	if stat.Size() < offset {
 		return ErrOffsetExceedsFileSize
 	}
-	fmt.Println(stat.Size())
 	if limit == 0 || limit+offset > stat.Size() {
 		limit = stat.Size() - offset
 	}
-	// Создаём файл
+
 	toFile, err := os.Create(toPath)
 	if err != nil {
 		return err
 	}
 	defer toFile.Close()
-	// Устанавливаем смещение
 	fromFile.Seek(offset, 0)
-	// Создаём буферизированные reader и writer
 	reader := bufio.NewReader(fromFile)
 	writer := bufio.NewWriter(toFile)
-	// Создаём прогресс бар
 	bar := pb.StartNew(int(limit))
-	// Побайтово копируем данные
-	// Я использовал ReadByte() для большей наглядности прогресс бара (он так более плавный)
-	// Намерено не использовал метод CopyN
-	// Знаю, что можно использовать io.reader.Read() и []Byte и ещё кучу вариантов
 	for i := 0; int64(i) < limit; i++ {
-		b, err := reader.ReadByte()                // Читаем байт
-		if err != nil && !errors.Is(err, io.EOF) { // Если ошибка и ошибка не End Of File, то возвращаем ошибку
+		b, err := reader.ReadByte()
+		if err != nil && !errors.Is(err, io.EOF) {
 			return err
 		}
-		if errors.Is(err, io.EOF) { // Если ошибка End Of File прерываем цикл, не записывая байт
+		if errors.Is(err, io.EOF) {
 			break
 		}
 		err = writer.WriteByte(b) // Записываем байт
 		if err != nil {
 			return err
 		}
-		bar.Add(1)                   // Добавляем единицу к прогресс бару
-		time.Sleep(time.Millisecond) // Для наглядности прогресс бара
+		bar.Add(1)
+		time.Sleep(time.Millisecond)
 	}
-	// Переносим данные из буффера
 	if err := writer.Flush(); err != nil {
 		return err
 	}
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"io"
-	"io/ioutil"
 	"os"
 	"testing"
 
@@ -11,27 +10,42 @@ import (
 
 func TestCopy(t *testing.T) {
 	t.Run("incorrect offset", func(t *testing.T) {
+		f, _ := os.CreateTemp("testdata", "out.txt")
+		defer os.Remove(f.Name())
 		err := Copy("testdata/input.txt", "testdata/out.txt", -100, 0)
 		assert.Equal(t, ErrIncorrectOffset, err)
+		f.Close()
 	})
 	t.Run("incorrect limit", func(t *testing.T) {
+		f, _ := os.CreateTemp("testdata", "out.txt")
+		defer os.Remove(f.Name())
 		err := Copy("testdata/input.txt", "testdata/out.txt", 0, -100)
 		assert.Equal(t, ErrIncorrectLimit, err)
+		f.Close()
 	})
 	t.Run("unsupported file", func(t *testing.T) {
+		f, _ := os.CreateTemp("testdata", "out.txt")
+		defer os.Remove(f.Name())
 		err := Copy("/dev/urandom", "testdata/out.txt", 0, 0)
 		assert.Equal(t, ErrUnsupportedFile, err)
+		f.Close()
 	})
 	t.Run("offset > length", func(t *testing.T) {
+		f, _ := os.CreateTemp("testdata", "out.txt")
+		defer os.Remove(f.Name())
 		err := Copy("testdata/input.txt", "testdata/out.txt", 50_000, 0)
 		assert.Equal(t, ErrOffsetExceedsFileSize, err)
+		f.Close()
 	})
 	t.Run("limit > length", func(t *testing.T) {
+		f, _ := os.CreateTemp("testdata", "out.txt")
+		defer os.Remove(f.Name())
 		err := Copy("testdata/input.txt", "testdata/out.txt", 0, 50_000)
 		filePtr, _ := os.Open("out.txt")
 		r := io.Reader(filePtr)
-		result, _ := ioutil.ReadAll(r)
+		result, _ := io.ReadAll(r)
 		assert.Equal(t, nil, err)
 		assert.Equal(t, []byte{}, result)
+		f.Close()
 	})
 }
